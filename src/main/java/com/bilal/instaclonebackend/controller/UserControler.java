be9b4e7b3c6352170f8bc0e3fba49a66a72656d8
AhamedBilal <bilalazmy0@gmail.com>
2020-12-05 14:19:13 +0530

    user get and post

--- src/main/java/com/bilal/instaclonebackend/controller/UserControler.java
+++ src/main/java/com/bilal/instaclonebackend/controller/UserControler.java
@@ -7,19 +7,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping(path = "/api/v1/user")
+@RequestMapping(path = "/api/user")
 class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping(path = "/register")
+    @PostMapping()
     public @ResponseBody
-    String addNewUser(@RequestParam String name, @RequestParam String email) {
-        User n = new User();
-        n.setName(name);
-        n.setEmail(email);
-        userRepository.save(n);
+    String addNewUser(User user) {
+        userRepository.save(user);
         return "Saved";
     }
 
