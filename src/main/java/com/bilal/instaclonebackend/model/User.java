f0ee352da65b9c6a8ad4b793df6f2fa08cf60790
AhamedBilal <bilalazmy0@gmail.com>
2020-12-05 14:56:34 +0530

    Validation added

--- src/main/java/com/bilal/instaclonebackend/model/User.java
+++ src/main/java/com/bilal/instaclonebackend/model/User.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
 
 
 @Entity
@@ -12,6 +13,7 @@ public class User {
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
     private String name;
+    @Email
     private String email;
     private String password;
 
